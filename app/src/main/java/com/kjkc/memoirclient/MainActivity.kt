7174ece8fd1cbf1b050eec9c546540e5f6847e32
Hokyun <h0kyungim@gmail.com>
2023-12-12 21:45:14 +0900

    init

--- app/src/main/java/com/kjkc/memoirclient/MainActivity.kt
+++ app/src/main/java/com/kjkc/memoirclient/MainActivity.kt
@@ -38,6 +38,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     MemoirClientTheme {
-        Greeting("Android")
+        Greeting("Kim jang kimchi")
     }
 }
