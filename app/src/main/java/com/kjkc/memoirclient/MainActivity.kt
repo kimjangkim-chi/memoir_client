15d21be28a71af24e9868d870dae0942c2a97eb8
h0kyun <h0kyungim@gmail.com>
2024-06-04 23:21:16 +0900

    feat: 화면 구성

--- app/src/main/java/com/kjkc/memoirclient/MainActivity.kt
+++ app/src/main/java/com/kjkc/memoirclient/MainActivity.kt
@@ -3,13 +3,28 @@ package com.kjkc.memoirclient
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Folder
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.kjkc.memoirclient.ui.theme.MemoirClientTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,8 +33,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             MemoirClientTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
                 }
             }
         }
@@ -27,10 +44,59 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
+fun App() {
+    Scaffold(
+        topBar = {
+            AppBar()
+        }
+    ) {
+        LazyColumn(contentPadding = it) {
+            items(3) {
+                Text(text = "test")
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppBar(modifier: Modifier = Modifier) {
+    TopAppBar(
+        title = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "APP_NAME",
+                    style = MaterialTheme.typography.displayMedium
+                )
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = "search",
+                    modifier = Modifier.size(30.dp)
+                )
+                Icon(
+                    imageVector = Icons.Filled.Add,
+                    contentDescription = "add",
+                    modifier = Modifier.size(30.dp)
+                )
+                Icon(
+                    imageVector = Icons.Filled.Settings,
+                    contentDescription = "setting",
+                    modifier = Modifier.size(30.dp)
+                )
+            }
+        },
+        navigationIcon = {
+            IconButton(onClick = { /*TODO*/ }, modifier = Modifier.size(30.dp)) {
+                Icon(
+                    imageVector = Icons.Filled.Folder,
+                    contentDescription = "folder",
+                )
+
+            }
+        },
+        modifier = modifier
     )
 }
 
@@ -38,6 +104,8 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     MemoirClientTheme {
-        Greeting("Kim jang kimchi")
+        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+            App()
+        }
     }
 }
