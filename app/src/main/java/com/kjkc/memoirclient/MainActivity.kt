0f54a0a07a46ffb545919612da122456d24b6012
Hokyun <h0kyungim@gmail.com>
2024-06-09 21:12:27 +0900

    feat: 메인화면 개발

--- app/src/main/java/com/kjkc/memoirclient/MainActivity.kt
+++ app/src/main/java/com/kjkc/memoirclient/MainActivity.kt
@@ -3,15 +3,24 @@ package com.kjkc.memoirclient
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Folder
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -23,9 +32,11 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.kjkc.memoirclient.ui.theme.MemoirClientTheme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,11 +59,52 @@ fun App() {
     Scaffold(
         topBar = {
             AppBar()
-        }
+        },
+        bottomBar = {
+            Button(
+                onClick = { /*TODO*/ },
+                shape = RoundedCornerShape(10.dp),
+                modifier = Modifier
+                    .padding(horizontal = 5.dp)
+                    .fillMaxWidth()
+            ) {
+                Text(text = "Let's Remind", style = MaterialTheme.typography.displayLarge)
+
+            }
+        },
+        containerColor = Color.Black
     ) {
-        LazyColumn(contentPadding = it) {
-            items(3) {
-                Text(text = "test")
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            userScrollEnabled = true,
+            contentPadding = PaddingValues(10.dp),
+            modifier = Modifier
+                .padding(
+                    start = 10.dp,
+                    end = 10.dp,
+                    top = it.calculateTopPadding() + 10.dp,
+                    bottom = it.calculateBottomPadding() + 20.dp
+                )
+                .background(Color.LightGray, RoundedCornerShape(10.dp))
+        ) {
+            items(30) {
+                Box(
+                    contentAlignment = Alignment.Center
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Folder,
+                        contentDescription = "folder",
+                        modifier = Modifier.size(100.dp),
+                        tint = Color(
+                            Random.nextInt(256),
+                            Random.nextInt(256),
+                            Random.nextInt(256),
+                            Random.nextInt(256)
+                        )
+                    )
+                    Text(text = "카테고리")
+                }
+
             }
         }
     }
@@ -64,27 +116,31 @@ fun AppBar(modifier: Modifier = Modifier) {
     TopAppBar(
         title = {
             Row(
-                verticalAlignment = Alignment.CenterVertically
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.fillMaxWidth()
             ) {
                 Text(
                     text = "APP_NAME",
-                    style = MaterialTheme.typography.displayMedium
+                    style = MaterialTheme.typography.displaySmall
                 )
-                Icon(
-                    imageVector = Icons.Default.Search,
-                    contentDescription = "search",
-                    modifier = Modifier.size(30.dp)
-                )
-                Icon(
-                    imageVector = Icons.Filled.Add,
-                    contentDescription = "add",
-                    modifier = Modifier.size(30.dp)
-                )
-                Icon(
-                    imageVector = Icons.Filled.Settings,
-                    contentDescription = "setting",
-                    modifier = Modifier.size(30.dp)
-                )
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(
+                        imageVector = Icons.Default.Search,
+                        contentDescription = "search",
+                        modifier = Modifier.size(30.dp)
+                    )
+                    Icon(
+                        imageVector = Icons.Filled.Add,
+                        contentDescription = "add",
+                        modifier = Modifier.size(30.dp)
+                    )
+                    Icon(
+                        imageVector = Icons.Filled.Settings,
+                        contentDescription = "setting",
+                        modifier = Modifier.size(30.dp)
+                    )
+                }
             }
         },
         navigationIcon = {
